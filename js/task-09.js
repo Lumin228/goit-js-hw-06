19e644b4075ad65ef32ec202d02023eadf3c8dc8
Lumin228 <6786679@gmail.com>
2023-11-07 10:26:55 +0200

    HW# 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+
+
+const buttn = document.querySelector(".change-color");
+const boddy = document.querySelector("body");
+const spann = document.querySelector(".color")
+
+buttn.addEventListener("click", () => {
+ 
+
+  const selectedColor =  `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  spann.textContent = `${selectedColor}`;
+  boddy.style.background = selectedColor;
+})
+
