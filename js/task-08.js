7b0a780c8ec54f75f974b54ab30beb7d9f6da877
Lumin228 <6786679@gmail.com>
2023-11-07 10:09:58 +0200

    HW#8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (login === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
